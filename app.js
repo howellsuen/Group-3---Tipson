feb6cb139d032f998fa915f3706b562a0cbd7acd
howellsuen <howell.suen@gmail.com>
2018-07-05 18:41:50 +0800

    add static public folder

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 const server = require('express-static')
 const app = express();
@@ -7,10 +5,10 @@ const app = express();
 const events = require('events');
 const util = require('util');
 
+app.use(express.static('public'));
 
-
-app.get('/', function (req, res) {
-    res.send(__dirname + 'index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(8080);
