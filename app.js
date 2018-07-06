0a2d3a7011db2d13bc75996272d9c65231b7d8d4
Jonathan Paita <jonathan.austria.paita@gmail.com>
2018-07-06 14:28:36 +0800

    added base dom

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const http = require('http');
 
-const express = require('express');
-const server = require('express-static')
+'strict';
+
+const express = require('express'); // http framework module
+const path = require('path'); // root module
+const bodyParser = require('body-parser'); // pull information from html in POST
+const nodeMailer = require('nodemailer');
+
 const app = express();
 
-const events = require('events');
-const util = require('util');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.get('/', function (req, res) {
-    res.send(__dirname + 'index.html');
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.listen(8080);
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+
+
+
+app.listen(3000);
+console.log('server is runnin on port 3000...');
