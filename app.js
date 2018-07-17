6b3892dfe6adfefd84b28fc64f272ae9d2e77bea
chisinhong <chisinhong@gmail.com>
2018-07-17 14:53:55 +0800

    facebook login

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const express = require('express'); // http framework module
 const path = require('path'); // root module
 const bodyParser = require('body-parser'); // pull information from html in POST
 const nodeMailer = require('nodemailer');
+const authRoutes = require('./routes/auth-routes'); //oauth routes
+const passportSetup = require('./config/passport-setup'); //link the passport-setup
+
 
 const app = express();
 
@@ -15,6 +18,9 @@ app.set('view engine', 'html');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//setup routes
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -36,8 +42,17 @@ app.get('/profile', (req, res) => {
     res.render('profile');
 });
 
+//https setting
+const https = require('https');
+const fs = require('fs');
+const options = {
+  cert: fs.readFileSync('./localhost.crt'),
+  key: fs.readFileSync('./localhost.key')
+};
 
+https.createServer(options, app).listen(3000);
 
-
+/*http setting
 app.listen(3000);
 console.log('server is runnin on port 3000...');
+*/
