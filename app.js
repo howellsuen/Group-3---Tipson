b02f3b074dcab14099643bd9a8be38eb49be2aea
chisinhong <chisinhong@gmail.com>
2018-07-17 23:02:04 +0800

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authRoutes = require('./routes/auth-routes');
+const authRoutes = require('./routers/auth-routes');
 const passportSetup = require('./config/passport-setup');
 
 // General Initialization
